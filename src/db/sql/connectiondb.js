7f37fc27b4e42bbc3ab5a69bf673c434b07387cb
JelleJ18 <151057272+JelleJ18@users.noreply.github.com>
2025-09-18 10:46:35 +0200

    fixed connectionissue

--- src/db/sql/connectiondb.js
+++ src/db/sql/connectiondb.js
@@ -6,11 +6,12 @@ const pool = mysql.createPool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONN_LIMIT),
   queueLimit: 0,
   namedPlaceholders: true,
+  ssl: {rejectUnauthorized: true}
 });
 
 module.exports = { pool };
